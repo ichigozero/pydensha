8f3c4da4f745966bed0108a411aef262b20ee6e9
Gary Sentosa <gary.sentosa@gmail.com>
2020-05-10 16:09:53 +0900

    Move try except block to decorator for DRY code

--- src/pydensha/pydensha.py
+++ src/pydensha/pydensha.py
@@ -1,26 +1,31 @@
 from gpiozero import RGBLED
 
 
+def _exc_attr_err(func):
+    def wrapper(*args, **kwargs):
+        try:
+            func(*args, **kwargs)
+        except AttributeError:
+            pass
+    return wrapper
+
+
 class PyDensha:
     def __init__(self, led_pins=None):
         self._led = None
 
         self.assign_led(led_pins)
 
+    @_exc_attr_err
     def assign_led(self, led_pins):
         self._close_led()
 
-        try:
-            self._led = RGBLED(
-                red=led_pins.get('red'),
-                green=led_pins.get('green'),
-                blue=led_pins.get('blue')
-            )
-        except AttributeError:
-            pass
+        self._led = RGBLED(
+            red=led_pins.get('red'),
+            green=led_pins.get('green'),
+            blue=led_pins.get('blue')
+        )
 
+    @_exc_attr_err
     def _close_led(self):
-        try:
-            self._led.close()
-        except AttributeError:
-            pass
+        self._led.close()
